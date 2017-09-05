eff426b25703b9352a35b42fab9254a399fc914e
YunluShi <yunlus@andrew.cmu.edu>
2017-09-05 13:10:17 -0700

    add chat

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ function closeNav() {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
@@ -22,6 +22,94 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
+    //console.log("hihi");
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+var me = {};
+// me.avatar = "https://lh6.googleusercontent.com/-lr2nyjhhjXw/AAAAAAAAAAI/AAAAAAAARmE/MdtfUmC0M4s/photo.jpg?sz=48";
+
+var you = {};
+// you.avatar = "https://a11.t26.net/taringa/avatares/9/1/2/F/7/8/Demon_King1/48x48_5C5.jpg";
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}            
+
+//-- No use time. It is a javaScript effect.
+function insertChat(who, chattext, time = 0){
+    var control = "";
+    var date = formatAMPM(new Date());
+    
+    if (who == "me"){
+        
+        control = '<li style="width:100%">' +
+                        '<div class="msj macro">' +
+                        '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ me.avatar +'" /></div>' +
+                            '<div class="chattext chattext-l">' +
+                                '<p>'+ chattext +'</p>' +
+                                '<p><small>'+date+'</small></p>' +
+                            '</div>' +
+                        '</div>' +
+                    '</li>';                    
+    }else{
+        control = '<li style="width:100%;">' +
+                        '<div class="msj-rta macro">' +
+                            '<div class="chattext chattext-r">' +
+                                '<p>'+chattext+'</p>' +
+                                '<p><small>'+date+'</small></p>' +
+                            '</div>' +
+                        '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="'+you.avatar+'" /></div>' +                                
+                  '</li>';
+    }
+    setTimeout(
+        function(){                        
+            $("ul").append(control);
+
+        }, time);
+    
+}
+
+function resetChat(){
+    $("ul").empty();
+}
+
+$(".mychattext").on("keyup", function(e){
+    if (e.which == 13){
+        var chattext = $(this).val();
+        if (chattext !== ""){
+            insertChat("me", chattext);              
+            $(this).val('');
+        }
+    }
+});
+
+//-- Clear Chat
+resetChat();
+
+//-- Print Messages
+// insertChat("me", "Hello, I'm glad you are here.", 0);
+// insertChat("me", "What do you want to know today?", 1500);    
+// insertChat("you", "Tell me more about yourself", 2500);
+// insertChat("me", "I am a UX designger, also an architect", 4000);
+// insertChat("you", "Tell me a joke",7000);
+// insertChat("me", "Spaceman: Computer! Computer! Do we bring battery?!", 9500);
+// insertChat("you", "LOL", 12000);
+insertChat("me", "Hello, I'm glad you are here.", 0);
+insertChat("me", "What do you want to know today?", 1500);    
+insertChat("you", "Tell me more about yourself.", 3500);
+insertChat("me", "I am a UX designger, also an architect. You are welcome to scroll down and view more in the 'About me' section.", 6000);
+insertChat("you", "Cool, can you tell me more about your recent works?",9000);
+insertChat("me", "Sure, please scroll down and click the 'Portfolio' section.", 12000);
+insertChat("you", "Thank you!", 14000);
+
+
+//-- NOTE: No use time on insertChat.
--- js/project_template.js
+++ js/project_template.js
@@ -1,50 +1,124 @@
-// Open and close sidebar
-function openNav() {
-    document.getElementById("mySidebar").style.width = "60%";
-    document.getElementById("mySidebar").style.display = "block";
-}
+// // Open and close sidebar
+// function openNav() {
+//     document.getElementById("mySidebar").style.width = "60%";
+//     document.getElementById("mySidebar").style.display = "block";
+// }
 
-function closeNav() {
-    document.getElementById("mySidebar").style.display = "none";
-}
-var slideIndex = 1;
-showDivs(slideIndex);
+// function closeNav() {
+//     document.getElementById("mySidebar").style.display = "none";
+// }
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
-function showProcess() {
-    var x = document.getElementById('myDIV');
-    if (x.style.display == 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   if (n > x.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";  
+// }
+// function showProcess() {
+//     var x = document.getElementById('myDIV');
+//     if (x.style.display == 'none') {
+//         x.style.display = 'block';
+//     } else {
+//         x.style.display = 'none';
+//     }
+// }
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {l
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("myBtn").style.display = "block";
+//     } else {
+//         document.getElementById("myBtn").style.display = "none";
+//     }
+// }
+
+// console.log("hihi");
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
+
+// Magnifier
+$(document).ready(function(){
+
+  var native_width = 0;
+  var native_height = 0;
+  console.log("hihi");  
+  $(".large").css("background","url('" + $(".small").attr("src") + "') no-repeat");
+  console.log("hihi1");
+  //Now the mousemove function
+  $(".magnify").mousemove(function(e){
+    //When the user hovers on the image, the script will first calculate
+    //the native dimensions if they don't exist. Only after the native dimensions
+    //are available, the script will show the zoomed version.
+    if(!native_width && !native_height)
+    {
+      //This will create a new image object with the same image as that in .small
+      //We cannot directly get the dimensions from .small because of the 
+      //width specified to 200px in the html. To get the actual dimensions we have
+      //created this image object.
+      var image_object = new Image();
+      image_object.src = $(".small").attr("src");
+      
+      //This code is wrapped in the .load function which is important.
+      //width and height of the object would return 0 if accessed before 
+      //the image gets loaded.
+      native_width = image_object.width;
+      native_height = image_object.height;
     }
-}
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
+    else
+    {
+      //x/y coordinates of the mouse
+      //This is the position of .magnify with respect to the document.
+      var magnify_offset = $(this).offset();
+      //We will deduct the positions of .magnify from the mouse positions with
+      //respect to the document to get the mouse positions with respect to the 
+      //container(.magnify)
+      var mx = e.pageX - magnify_offset.left;
+      var my = e.pageY - magnify_offset.top;
+      
+      //Finally the code to fade out the glass if the mouse is outside the container
+      if(mx < $(this).width() && my < $(this).height() && mx > 0 && my > 0)
+      {
+        $(".large").fadeIn(100);
+      }
+      else
+      {
+        $(".large").fadeOut(100);
+      }
+      if($(".large").is(":visible"))
+      {
+        //The background position of .large will be changed according to the position
+        //of the mouse over the .small image. So we will get the ratio of the pixel
+        //under the mouse pointer with respect to the image and use that to position the 
+        //large image inside the magnifying glass
+        var rx = Math.round(mx/$(".small").width()*native_width - $(".large").width()/2)*-1;
+        var ry = Math.round(my/$(".small").height()*native_height - $(".large").height()/2)*-1;
+        var bgp = rx + "px " + ry + "px";
+        
+        //Time to move the magnifying glass with the mouse
+        var px = mx - $(".large").width()/2;
+        var py = my - $(".large").height()/2;
+        //Now the glass moves with the mouse
+        //The logic is to deduct half of the glass's width and height from the 
+        //mouse coordinates to place it with its center at the mouse coordinates
+        
+        //If you hover on the image now, you should see the magnifying glass in action
+        $(".large").css({left: px, top: py, backgroundPosition: bgp});
+      }
     }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+  })
+})
